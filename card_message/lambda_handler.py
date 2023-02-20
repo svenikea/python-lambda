33b872e5568ac87dcdbc459f77e8d8988e194465
svenikea <nguyenhongky1998@gmail.com>
2023-02-20 23:16:10 +0700

    added deploy_status and color condition

--- card_message/lambda_handler.py
+++ card_message/lambda_handler.py
@@ -1,31 +1,35 @@
-import json, boto3, math, os
+import json, boto3, os
 from botocore.vendored import requests
 
 codecommit = boto3.client('codecommit')
+codepipeline = boto3.client('codepipeline')
 domain = os.environ['domain']
 region = os.environ['region']
 webhook =  os.environ['webhook']
-color = "#ffff00"
-status = "Deploying"
+color = None
 pipeline_name = os.environ['pipeline_name']
 image = os.environ['image_url']
 
 def lambda_handler(event, context):
-    # TODO implement
-
+    # Repository detail
     references_raw = { reference['ref'] for reference in event['Records'][0]['codecommit']['references'] }
     reference = str(references_raw).replace("'","").replace("{","").replace("}","").split("/")
     commit_raw = { reference['commit'] for reference in event['Records'][0]['codecommit']['references'] }
     long_commit_id = str(commit_raw).replace("'","").replace("{","").replace("}","")
     short_commit_id = long_commit_id[0:6]
-
-    
-    #Get the repository from the event and show its git clone URL
     repository_raw = event['Records'][0]['eventSourceARN'].split(':')[5]
     repository = codecommit.get_repository(repositoryName=repository_raw)['repositoryMetadata']['repositoryName']
     commit_message = codecommit.get_commit(commitId=long_commit_id,repositoryName=repository)['commit']['message']
-    #payload = {"text" : f"<users/all> Deploying {domain} commit: *<https://{region}.console.aws.amazon.com/codesuite/codecommit/repositories/{repository}/commit/{long_commit_id}?region={region}|{short_commit_id}>* message: {commit_message}"}
-    
+
+    # Get Deploy Stage status on CodePipeline
+    deploy_status = codepipeline.get_pipeline_state(name=pipeline_name)['stageStates'][1]['actionStates'][0]['latestExecution']['status']
+    if deploy_status == "Succeeded":
+        color = "#ffff00"
+    elif deploy_status == "Failed":
+        color = "#FF0000"
+    elif deploy_status == "Cancelled" or deploy_status == "Stopped":
+        color = "#FFA500"
+
     payload = {
             "cards": [
                 {
@@ -40,7 +44,7 @@ def lambda_handler(event, context):
                             "widgets": [
                                 {
                                     "textParagraph": {
-                                        "text": f"<b>Domain: </b>{domain}<br><b>Commit-id:</b> <a href=https://{region}.console.aws.amazon.com/codesuite/codecommit/repositories/{repository}/commit/{long_commit_id}?region={region}>{short_commit_id}</a><br><b>Branch: </b>{reference[2]}<br><b>Build Status: </b> <font color=\"{color}\">{status}</font><br>"
+                                        "text": f"<b>Domain: </b>{domain}<br><b>Commit-id:</b> <a href=https://{region}.console.aws.amazon.com/codesuite/codecommit/repositories/{repository}/commit/{long_commit_id}?region={region}>{short_commit_id}</a><br><b>Message: </b>{commit_message}<br><b>Status: </b> <font color=\"{color}\">{deploy_status}</font><br>"
                                     },
                                     "buttons": [
                                         {
@@ -48,7 +52,7 @@ def lambda_handler(event, context):
                                                 "text": "Pipeline Details",
                                                 "onClick": {
                                                     "openLink": {
-                                                        "url": "https://{region}.console.aws.amazon.com/codesuite/codepipeline/pipelines/{pipeline_name}/view?region={region}"
+                                                        "url": f"https://{region}.console.aws.amazon.com/codesuite/codepipeline/pipelines/local-minc-connect-pipeline-ec2/view?region={region}"
                                                     }
                                                 }
                                             }
@@ -62,26 +66,17 @@ def lambda_handler(event, context):
             ]
         }
     try:
-        #response = codecommit.get_repository(repositoryName=repository)
-        print(f"Repo Name: {repository}")
-        print(f"Branch: {reference[2]}")
-        print(f"Long Commit: {long_commit_id}")
-        print(f"Short Commit: {short_commit_id}")
-        print(f"Message: {commit_message}")
-        print(f"Payload: {payload}")
-        requests.post(webhook, json=payload)
-        #print("Clone URL: " +response['repositoryMetadata']['cloneUrlHttp'])
-        # return response['repositoryMetadata']['cloneUrlHttp']
-        requests.post(webhook, json=payload)
-        # print(f"Short Commit: {short_commit_id}")
-        # print(f"Long commit: {long_commit_id}")
-        # print(f"Message: {commit_message}")
+        #print(f"Repo Name: {repository}")
+        #print(f"Branch: {reference[2]}")
+        #print(f"Long Commit: {long_commit_id}")
+        #print(f"Short Commit: {short_commit_id}")
+        #print(f"Message: {commit_message}")
+        #print(f"Payload: {payload}")
+        #print(response)
+        #print(deploy_status)
+        response = requests.post(webhook, json=payload)
 
     except Exception as e:
         print(e)
         print('Error getting repository {}. Make sure it exists and that your repository is in the same region as this function.'.format(repository))
         raise e
-#    return {
-#        'statusCode': 200,
-#        'body': json.dumps('Hello from Lambda!')
-#    }
